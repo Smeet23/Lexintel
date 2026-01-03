7eb93398e1559893ca158da5634504c959f560a8
Smeet Agrawal <smeetagrawal23@gmail.com>
2026-01-03 19:37:49 +0530

    chore: add models package exports

---
+++ packages/shared/src/shared/models/__init__.py
@@ -0,0 +1,11 @@
+from .base import Base
+from .case import Case
+from .document import Document, DocumentChunk, ProcessingStatus
+
+__all__ = [
+    "Base",
+    "Case",
+    "Document",
+    "DocumentChunk",
+    "ProcessingStatus",
+]
